993023c8dd33bdc5970172bd4c39b718c79d736a
andreacassini <andrea1316@hotmail.it>
2023-07-18 12:40:47 +0200

    create tutte le classi

--- models/ComunicationsSystem.php
+++ models/ComunicationsSystem.php
@@ -4,11 +4,56 @@ class ComunicationsSystem
     private $sender;
     private $receiver;
     private $title;
-    
+    private $content;
+    public $delivered = false;
+    public static $sound = 'DRIIN';
 
-    public function __construct($name, $content)
+    //SENDER
+    public function send()
     {
-        $this->name = $name;
+        $this->delivered = true;
+    }
+    public function setSender($sender)
+    {
+        $this->sender = $sender;
+    }
+    public function getSender()
+    {
+        return $this->sender;
+    }
+
+    //RECEIVER
+    public function setReceiver($receiver)
+    {
+        $this->receiver = $receiver;
+    }
+    public function getReceiver()
+    {
+        return $this->receiver;
+    }
+
+    //TITLE
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    //CONTENT
+    public function setContent($content)
+    {
         $this->content = $content;
+    }
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    public function sent()
+    {
+        return 'Invio effettuato';
     }
 }
